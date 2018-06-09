fbfe5e0e970426aaca6e97703ab3b61380321eb7
Prashant Sabnekar <mistersps@gmail.com>
2018-06-09 07:50:15 +0530

    controller

--- src/main/java/com/prashant/controllers/IpMonitoringController.java
+++ src/main/java/com/prashant/controllers/IpMonitoringController.java
@@ -1,7 +1,11 @@
 package com.prashant.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.prashant.model.IpConfigDTO;
+import com.prashant.service.IPMonitoringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author prashant sabnekar
@@ -15,4 +19,36 @@ public class IpMonitoringController {
     public String checkApplicationIsRunning() {
         return "The IP Monitoring Application is Up & Running";
     }
+
+    @Autowired
+    private IPMonitoringService service;
+
+    @RequestMapping(value="/configurations", method = RequestMethod.GET)
+    public String getAllCOnfigurations(){
+        return service.getAllConfigurations();
+    }
+
+    @RequestMapping(value = "/configuration", method = RequestMethod.POST)
+    public ResponseEntity< String > persistIpConfiguration(@RequestBody String ipConfig) {
+        if (service.isValid(ipConfig)) {
+            service.persist(ipConfig);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
+    }
+
+    @RequestMapping(value = "/configuration", method = RequestMethod.DELETE)
+    public ResponseEntity< String > deleteIpConfiguration(@RequestBody String ipConfig) {
+        if (service.isValid(ipConfig)) {
+            service.delete(ipConfig);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
+    }
+
+    @RequestMapping(value="/configuration/{ip}", method = RequestMethod.GET)
+    public String checkCOnfigurations(@PathVariable("ip") String ipAddress){
+        return service.isIpPresent(ipAddress);
+    }
+
 }
